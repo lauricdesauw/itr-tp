2eed55603da27f8a8c276c55bc6555025a00ef99
Desauw <lauric@Jace.localdomain>
2020-02-19 12:03:09 +0100

    Update bump both

--- src/bumper.c
+++ src/bumper.c
@@ -6,7 +6,7 @@ int touch_right = 0x10;
 
 DeclareTask(task_bump_right);
 DeclareTask(task_bump_left);
-
+DeclareTask(task_bump_both);
 
 
 TASK task_bump_right {
@@ -23,18 +23,33 @@ TASK task_bump_left {
         SCHEDULE = FULL;
 };
 
+TASK task_bump_bothx {
+        PRIORITY = 1;
+        AUTOSTART = FALSE;
+        ACTIVATION = 1;
+        SCHEDULE = FULL;
+};
 
 // Code C
 TASK(task_bump_right)
 {
-    turn_left(40);
+    turn_left(100);
     ChainTask(taskmain);
     TerminateTask();
 }
 
 TASK(task_bump_left)
 {
-  turn_right(40);
+  turn_right(100);
+  ChainTask(taskmain);
+  TerminateTask();
+}
+
+
+TASK(task_bump_both)
+{
+  go_back(150);
+  turn_right(100);
   ChainTask(taskmain);
   TerminateTask();
 }
